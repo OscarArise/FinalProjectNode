a659eecd5f3cbbc9d535c2ada7aefda3f4dbcb7e
yahir <pouputopou4@gmail.com>
2025-06-16 14:49:30 -0600

    api qr

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// Importar rutas existentes
 const contactoRoutes = require('./routes/contactos');
 const solicitudRoutes = require('./routes/solicitudes');
-
+// Importar la ruta del QR
+const qrRoutes = require('./routes/qr');
 
 app.use(cors());
 app.use(express.json());
-//La ruta base /api/empleados se configura explícitamente  
-//Todo lo que esté en el archivo routes/empleados.js se va a usar bajo la ruta /api/empleados
-//app.use('/api/empleados', itemRoutes);
+app.use(bodyParser.json());
 
-app.use(bodyParser.json()); 
+// Montar rutas
 app.use('/api/contactos', contactoRoutes);
 app.use('/api/solicitudes', solicitudRoutes);
+// Montar la ruta de QR: GET /api/qr/:id
+app.use('/api/qr', qrRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
