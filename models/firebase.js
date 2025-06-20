b398811a80d75ada92c877d6f5c4a7c5a8a04c41
Natsui122 <saulagaspar@gmail.com>
2025-06-20 04:00:19 -0600

    preparacion de trabajo en proyecto

--- models/firebase.js
+++ models/firebase.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./pruebasfirebase-c2278-firebase-adminsdk-fbsvc-97b8b5341c.json');
+const serviceAccount = require('../config/firebase-key.json');
 
 initializeApp({
   credential: cert(serviceAccount)
